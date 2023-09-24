a9d7a01aa48c4d8ae9bf06541e4b7c0a84fcb160
heyitsmeposi <heyitsmeposi@gmail.com>
2023-09-23 17:10:51 -0700

    Setup basic authentication

--- routes/auth.js
+++ routes/auth.js
@@ -10,11 +10,56 @@ passport.use(new GoogleStrategy({
     callbackURL: '/oauth2/redirect/google',
     scope: ['profile']
 }, async function verify(issuer, profile, cb) {
+    console.log("Verify Starting...");
+    const userQuery = await db.query("SELECT * FROM federated_credentials WHERE provider = $1 AND subject = $2", [
+        issuer,
+        profile.id
+    ]);
 
+    if (userQuery.rows.length === 0) {
+        db.query("INSERT INTO users (name) VALUES ($1) RETURNING id", [
+            profile.displayName
+        ], async (err, result) => {
+            if (err) { return cb(err) }
+            console.log(result);
+            await db.query("INSERT INTO federated_credentials (user_id, provider, subject) VALUES ($1, $2, $3)", [
+                result.rows[0].id,
+                issuer,
+                profile.id
+            ]);
+
+            return cb(null, { id: result.rows[0].id, name: profile.displayname });
+        });
+
+    } else {
+        db.query("SELECT * FROM users WHERE id = $1", [
+            userQuery.rows[0].user_id
+        ], (err, result) => {
+            if (err) { return cb(err); }
+            return cb(null, userQuery.rows[0]);
+        });
+    }
 }));
+
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, user);
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
 
 const router = express.Router();
 
 router.get("/login/federated/google", passport.authenticate("google"));
+
+router.get('/oauth2/redirect/google', passport.authenticate('google', {
+    successRedirect: 'http://localhost:3000/testLogin/',
+    failureRedirect: '/login'
+}));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,42 @@
+require("dotenv").config();
 const express = require("express");
+const session = require("express-session");
+const pgSession = require("connect-pg-simple")(session);
+const passport = require("passport");
+const pool = require("./db");
 const app = express();
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        res.redirect("/notAuthenticated");
+    }
+}
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new pgSession({
+        pool: pool,
+        tableName: "session"
+    })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const authRouter = require("./routes/auth");
 
+app.use("/testLogin", ensureAuthenticated, (req, res) => {
+    res.send("AUTHENTICATED");
+});
+app.use("/notAuthenticated", (req, res) => {
+    res.send("NOT AUTHENTICATED");
+})
 app.use("/", authRouter);
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server listening...");
